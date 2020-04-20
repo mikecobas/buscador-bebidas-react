7bb9adb634122637f4ce61222953115d9acc660d
Miguel Cobas <mcobas@dealergeek.com>
2020-04-19 23:16:44 -0500

    Se creo el context para las recetas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, {Fragment} from 'react';
-import Header from './components/Header'
-import Formulario from './components/Formulario'
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
-import CategoriasProvider from './context/CategoriasContext'
-
+import CategoriasProvider from "./context/CategoriasContext";
+import RecetasProvider from "./context/RecetasContext";
 
 function App() {
   return (
-
     <CategoriasProvider>
-      <Header/>
+      <RecetasProvider>
+        <Header />
 
-      <div className="container mt-5">
-        <div className="row">
-          <Formulario />
+        <div className="container mt-5">
+          <div className="row">
+            <Formulario />
+          </div>
         </div>
-      </div>
+      </RecetasProvider>
     </CategoriasProvider>
   );
 }

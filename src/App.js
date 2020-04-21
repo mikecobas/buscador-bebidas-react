4063ebd9d6bdd1536532c821267aa96338a3aca8
Miguel Cobas <mcobas@dealergeek.com>
2020-04-21 02:39:22 -0500

    Se agrego el componente de receta y a su vez el context para ese componente

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import ListaRecetas from "./components/ListaRecetas";
 
 import CategoriasProvider from "./context/CategoriasContext";
 import RecetasProvider from "./context/RecetasContext";
+import ModalProvider from "./context/ModalContext";
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
-        <Header />
+        <ModalProvider>
+          <Header />
 
-        <div className="container mt-5">
-          <div className="row">
-            <Formulario />
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
+            <ListaRecetas />
           </div>
-        </div>
+        </ModalProvider>
       </RecetasProvider>
     </CategoriasProvider>
   );
